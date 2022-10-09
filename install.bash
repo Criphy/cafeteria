0f48d2d4c6355977ea81650f6f248a519c0a4510
Сергей <serega13epytb@gmail.com>
2022-10-09 23:16:29 +0400

    Updated installer

--- install.bash
+++ install.bash
@@ -5,8 +5,8 @@ if [ "$SYSTEM" == "Linux" ]; then
   pkg install python postgresql
   pip install -r requirements.txt
   chmod +x ./scripts/main.py
-  sudo export CAFE="$(pwd)/scripts/main.py"
-  sudo export CAFEID="$(pwd)/scripts/id"
+  export CAFE="$(pwd)/scripts/main.py"
+  export CAFEID="$(pwd)/scripts/id"
   echo "Anything is installed! GG."
   echo "Command to start the game - python \$CAFE"
 else
