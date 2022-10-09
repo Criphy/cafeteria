a454e63e20a55bcde95c4eb5ffcaf19a3e9c42f0
Сергей <serega13epytb@gmail.com>
2022-10-09 22:30:53 +0400

    Updated installer

--- install.bash
+++ install.bash
@@ -3,8 +3,7 @@ rm -rf LICENSE README.md
 pkg install python postgresql
 pip install -r requirements.txt
 rm -rf requirements.txt
-mv ./scripts/main.py ./
 chmod +x main.py
-set CAFETERIA="$(pwd)/main.py"
+set CAFETERIA="$(pwd)/scripts/main.py"
 echo "Anything is installed! GG."
-echo "Command to start the game - python \$CAFETERIA"
+echo "Command to start the game - python $CAFETERIA"
