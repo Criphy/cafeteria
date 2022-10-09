6642a7a4479c68f7f6f4d2ea44a4a836250490d5
Сергей <serega13epytb@gmail.com>
2022-10-09 22:42:55 +0400

    Updated installer

--- install.bash
+++ install.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
-rm -rf LICENSE README.md
-pkg install python postgresql
-pip install -r requirements.txt
-rm -rf requirements.txt
-chmod +x main.py
-CAFETERIA="$(pwd)/scripts/main.py"
-echo "Anything is installed! GG."
-echo "Command to start the game - python $CAFETERIA"
+if [ "$(uname)" == "linux" ]; then
+  rm -rf LICENSE README.md
+  pkg install python postgresql
+  pip install -r requirements.txt
+  rm -rf requirements.txt
+  chmod +x main.py
+  CAFETERIA="$(pwd)/scripts/main.py"
+  echo "Anything is installed! GG."
+  echo "Command to start the game - python $CAFETERIA"
+else
+  echo "Installer can't starts on this system."
+fi
