528c8235c0f4de60af1edd335997a2a2a4c57173
Сергей <serega13epytb@gmail.com>
2022-10-09 23:35:40 +0400

    Cafeteria (v1.0.0)

--- install.bash
+++ install.bash
@@ -2,12 +2,12 @@
 SYSTEM="$(uname)"
 if [ "$SYSTEM" == "Linux" ]; then
   rm -rf LICENSE README.md
-  pkg install python postgresql
+  pkg install python postgresql sudo
   pip install -r requirements.txt
   chmod +x ./scripts/main.py
   CAFE="$(pwd)/scripts/main.py"
   echo "Anything is installed! GG."
-  echo "Command to start the game - python $CAFE"
+  echo "Command to start the game - sudo python $CAFE"
 else
   echo "Installer can't starts on this system."
 fi
