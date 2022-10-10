09953e250aed70e76dd0207526d25e7468741117
Сергей <serega13epytb@gmail.com>
2022-10-10 14:29:22 +0400

    Updated installer

--- install.bash
+++ install.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
-SYSTEM="$(uname)"
-if [ "$SYSTEM" == "Linux" ]; then
+if [ "$(uname)" == "Linux" ]; then
   rm -rf LICENSE README.md
-  pkg install python postgresql tsu
+  pkg install python postgresql tsu unzip
   pip install -r requirements.txt
+  wget -o Cafeteria_Scripts.zip https://drive.google.com/file/d/1uOQ9RHqDXSuOqiexYOYOKCQIlyUVbR5N
+  unzip Cafeteria_Scripts.zip -d ./scripts
   chmod +x ./scripts/main.py
   CAFE="$(pwd)/scripts/main.py"
   echo "Anything is installed! GG."
