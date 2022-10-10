4012eb078137123dc067d83b817fb496e3603e14
Сергей <serega13epytb@gmail.com>
2022-10-10 14:49:28 +0400

    Updated installer

--- install.bash
+++ install.bash
@@ -3,8 +3,9 @@ if [ "$(uname)" == "Linux" ]; then
   rm -rf LICENSE README.md
   pkg install python postgresql tsu unzip
   pip install -r requirements.txt
-  curl -L -o Cafeteria_Scripts.zip 'https://drive.google.com/uc?export=download&id=1uOQ9RHqDXSuOqiexYOYOKCQIlyUVbR5N'
-  unzip Cafeteria_Scripts.zip -d ./scripts
+  curl -L -o CafeFiles.zip 'https://drive.google.com/uc?export=download&id=1uOQ9RHqDXSuOqiexYOYOKCQIlyUVbR5N'
+  unzip CafeFiles.zip -d ./scripts
+  rm -rf CafeFiles.zip
   chmod +x ./scripts/main.py
   CAFE="$(pwd)/scripts/main.py"
   echo "Anything is installed! GG."
